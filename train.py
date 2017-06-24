0aade53a3b42497dec387c951a9bd2b48903e731
hjimce <1056546436@qq.com>
2017-06-24 12:09:45 +0800

    train

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ def parse_args():
                         choices=['vgg16_ssd_300', 'vgg16_ssd_512'], help='which network to use')
     parser.add_argument('--batch-size', dest='batch_size', type=int, default=32,
                         help='training batch size')
-    parser.add_argument('--resume', dest='resume', type=int, default=86,
+    parser.add_argument('--resume', dest='resume', type=int, default=-1,
                         help='resume training from epoch n')
     parser.add_argument('--finetune', dest='finetune', type=int, default=-1,
                         help='finetune from epoch n, rename the model before doing this')
